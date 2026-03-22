2892718124a3d73adcbe363726dd08b028d22aa8
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-03-22 12:56:03 -0300

    .

---
+++ script.js
@@ -0,0 +1,22 @@
+let entrada = document.querySelector('#entrada');
+let pasta = document.querySelector('#pasta');
+let pizza = document.querySelector('#pizza');
+let sobremesa = document.querySelector('#sobremesa');
+
+if (entrada.)
+
+function entrada() {
+
+}
+
+function pizza() {
+
+}
+
+function massa() {
+    
+}
+
+function sobremesa() {
+
+}
