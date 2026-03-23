e5148db20f9b6a9b054a092b0e1b6970077f88d5
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-03-23 18:42:34 -0300

    .

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-let entrada = document.querySelector('#entrada');
-let pasta = document.querySelector('#pasta');
-let pizza = document.querySelector('#pizza');
-let sobremesa = document.querySelector('#sobremesa');
+let entradas = document.querySelector('#entrada');
+let pastas = document.querySelector('#pasta');
+let pizzas = document.querySelector('#pizza');
+let sobremesas = document.querySelector('#sobremesa');
 
-if (entrada.)
+let cardapio = document.querySelectorAll('.cardapio');
 
 function entrada() {
+    pizzas.style.display = 'none';
+    pastas.style.display = 'none';
+    sobremesas.style.display = 'none';
 
+    entradas.style.display = 'flex';
+
+    cardapio.style = 'background-image linear-gradient(rgba(189,105,105,0.6), rgba(189, 105, 105, 0.6)), url("img/background-red.png")';
 }
 
 function pizza() {
+    entradas.style.display = 'none';
+    pastas.style.display = 'none';
+    sobremesas.style.display = 'none';
 
+    pizzas.style.display = 'flex';
 }
 
 function massa() {
-    
+    pizzas.style.display = 'none';
+    entradas.style.display = 'none';
+    sobremesas.style.display = 'none';
+
+    pastas.style.display = 'flex';
 }
 
 function sobremesa() {
+    entradas.style.display = 'none';
+    pastas.style.display = 'none';
+    pizzas.style.display = 'none';
 
+    sobremesas.style.display = 'flex';
 }
